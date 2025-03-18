df4bb81668e1d289bc1c916f3b3574450da4636b
Sian <arki13@naver.com>
2025-03-18 22:25:53 +0900

    Update and rename File_Handling.py to Convert file encoding cp949 to utf8.py
    rename file

--- File_Handling.py
+++
@@ -1,7 +0,0 @@
-# Convert file encoding cp949 to utf8
-	# Linux Command
-	iconv -f cp949 -t utf-8 "Original File Name" > "Copied File Name"
-	
-	# Run on Python
-	import subprocess
-	subprocess.check_output("iconv -f cp949 -t utf-8 'Original File Name' > 'Copied File Name'", shell=True)
